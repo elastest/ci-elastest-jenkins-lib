ce26ce518d7ea3066db6fadd6584b3e4a155dd0f
gtunon <gtunon@naevatec.com>
2017-10-05 14:32:52 +0200

    encapsulate start elastest

--- vars/ciElastestConnect.groovy
+++ vars/ciElastestConnect.groovy
@@ -1,3 +1,18 @@
+def startElastest(){
+	def start_elastest_result = sh  script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start  --forcepull --nocheck', returnStatus:true
+	echo 'start_elastest_result = '+start_elastest_result
+
+	if (start_elastest_result == 0){
+		elastest_is_running = sh  script: 'python ci-elastest-jenkins-lib/scripts/checkETM.py', returnStatus:true
+		echo 'elastest_is_running = '+elastest_is_running
+	}
+	else {
+		def stop_elastest_result = sh  script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform stop --forcepull --nocheck', returnStatus:true
+	}
+	
+	return start_elastest_result
+}
+
 def call(body) {
 	def config = [:] //values for configure the job
 					 //in a future version of the components? by default latest
@@ -8,15 +23,10 @@ def call(body) {
 	//check the execution ${sharedElastest}
 		
 		
-	if ( '${config.sharedElastest}' ){
+	if ( "$SHARED_ELASTEST" ){
 		node ('sharedElastest'){
-			stage ('launch elastest')
-				sh "echo 'shared ${config.sharedElastest} ...'"
-				sh "echo 'shared ${SHARED_ELASTEST} ...'"
-				sh "echo 'shared $SHARED_ELASTEST ...'"
-				echo "shared $SHARED_ELASTEST"
-				
-				echo ('sharedElastest')
+			stage ('launch elastest' )			
+				echo ('sharedElastest=$SHARED_ELASTEST')
 				echo ('retrieve scripts')
 				
 				sh 'if cd ci-elastest-jenkins-lib; then git pull; else git clone https://github.com/elastest/ci-elastest-jenkins-lib.git ci-elastest-jenkins-lib; fi'
@@ -30,16 +40,7 @@ def call(body) {
 				if (elastest_is_running != 0 ){
 					echo 'ElasTest is not running...'
 					echo 'START Shared ElasTest'
-					def start_elastest_result = sh  script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start  --forcepull --nocheck', returnStatus:true
-					echo 'start_elastest_result = '+start_elastest_result
-
-					if (start_elastest_result == 0){
-						elastest_is_running = sh  script: 'python ci-elastest-jenkins-lib/scripts/checkETM.py', returnStatus:true
-						echo 'elastest_is_running = '+elastest_is_running
-					}
-					else {
-						def stop_elastest_result = sh  script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform stop --forcepull --nocheck', returnStatus:true
-					}
+					elastest_is_running = startElastest()
 				}
 				else {
 					echo 'TODO: provide elastest feedback'
@@ -54,9 +55,10 @@ def call(body) {
 	else {
 		node('commonE2E'){
 			stage ('launch elastest')
-				sh "echo 'shared ${config.sharedElastest} ...'"
-				echo ('NOT sharedElastest')
+				echo ('sharedElastest=$SHARED_ELASTEST')
 				echo ('TODO: run elastest')
+				def elastest_is_running = startElastest()
+				
 				echo ('TODO: provide elastest feedback')
 				
 			//body of the pipeline	
