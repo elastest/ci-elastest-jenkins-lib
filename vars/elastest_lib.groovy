31aa55a14fff27f6e7b08a8c6a90176df4fe8ba6
gtunon <gtunon@naevatec.com>
2017-10-11 15:10:58 +0200

    change --forcepull to --pullcore

--- vars/elastest_lib.groovy
+++ vars/elastest_lib.groovy
@@ -50,7 +50,7 @@ class elastest_lib implements Serializable {
 	def startElastest(){
 		echo '[INI] startElastest'
 		
-		def start_elastest_result = this.@ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start --forcepull '+ lite, returnStatus:true
+		def start_elastest_result = this.@ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start --pullcore '+ lite, returnStatus:true
 		echo 'startElastest-- start_elastest_result = '+start_elastest_result
 		
 		echo '[END] startElastest'
