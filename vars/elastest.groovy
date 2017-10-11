c55e1335c227865d3efb0ef2c0f4937e00fc1be8
gtunon <gtunon@naevatec.com>
2017-10-11 08:38:06 +0200

    this.ctx to this.@ctx

--- vars/elastest.groovy
+++ vars/elastest.groovy
@@ -14,7 +14,7 @@ class elastest implements Serializable {
 	def startElastest(){
 		echo '[INI] startElastest'
 		
-		def start_elastest_result = this.ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start --forcepull '+ lite, returnStatus:true
+		def start_elastest_result = this.@ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform start --forcepull '+ lite, returnStatus:true
 		echo 'startElastest-- start_elastest_result = '+start_elastest_result
 		
 		echo '[END] startElastest'
@@ -23,29 +23,29 @@ class elastest implements Serializable {
 	
 	def elastestIsRunning(){
 		echo '[INI] elastestIsRunning'
-		def platform_state = this.ctx.sh script: 'docker ps | grep elastest_platform | grep -c Up', returnStatus:true
-		def etm_state = this.ctx.sh script: 'docker ps | grep etm_1 | grep -c Up', returnStatus:true
+		def platform_state = this.@ctx.sh script: 'docker ps | grep elastest_platform | grep -c Up', returnStatus:true
+		def etm_state = this.@ctx.sh script: 'docker ps | grep etm_1 | grep -c Up', returnStatus:true
 		echo '[END] elastestIsRunning'
 		return (platform_state==0 && etm_state==0)
 	}
 
 	def waitElastest(){
 		echo '[INI] waitElastest'
-		def elastest_is_running = this.ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform wait', returnStatus:true
+		def elastest_is_running = this.@ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform wait', returnStatus:true
 		echo '[END] waitElastest'
 		return (elastest_is_running == 0)
 	}
 
 	def stopElastest(){
 		echo '[INI] stopElastest'
-		def start_elastest_result = this.ctx.sh script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform stop', returnStatus:true
+		def start_elastest_result = this.@ctx.sh script: 'docker run -d -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform stop', returnStatus:true
 		echo 'start_elastest_result = '+start_elastest_result	
 		echo '[END] stopElastest'
 	}
 
 	def getAPI(){
 		echo '[INI] getAPI'
-		def get_api = this.ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform inspect --api', returnStatus:true
+		def get_api = this.@ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform inspect --api', returnStatus:true
 		echo '[END] getAPI'
 	}
 	
@@ -59,8 +59,8 @@ class elastest implements Serializable {
 		println ("BODY: "+this.@shared)
 		
 		if ( this.@shared == true ){
-			this.ctx.node ('this.ctx.sharedElastest'){
-				this.ctx.stage ('launch elastest' )			
+			this.@ctx.node ('this.@ctx.sharedElastest'){
+				this.@ctx.stage ('launch elastest' )			
 					echo "sharedElastest ="+this.@shared
 									
 					def elastest_is_running = elastestIsRunning()
@@ -75,11 +75,11 @@ class elastest implements Serializable {
 						while (!elastest_is_running && counter > 0){
 							elastest_is_running = waitElastest()
 							counter = counter -1
-							def return_status = this.ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
+							def return_status = this.@ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
 							echo 'elastest_is_running:'+elastest_is_running
 						}
 							
-						def return_status = this.ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
+						def return_status = this.@ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
 						if (! elastest_is_running){
 							currentBuild.result = 'FAILURE'
 							return
@@ -93,16 +93,16 @@ class elastest implements Serializable {
 				body();	
 				echo '[END] User stages'
 				
-				this.ctx.stage ('release elastest')
+				this.@ctx.stage ('release elastest')
 					echo ('Shared elastest wont be ende because other jobs would be using it')
 			}
 		}
 		else {
-			this.ctx.node('commonE2E'){
-				this.ctx.stage ('launch elastest')
+			this.@ctx.node('commonE2E'){
+				this.@ctx.stage ('launch elastest')
 					echo "sharedElastest ="+this.@shared
 					def elastest_is_running = elastestIsRunning()
-					if (elastest_is_running){ //stop and start again --> elastest is unique and frethis.ctx.sh with each start
+					if (elastest_is_running){ //stop and start again --> elastest is unique and frethis.@ctx.sh with each start
 						stopElastest()
 						sleep(10)//TODO: change for method like waitToStop or something like that
 						elastest_is_running = elastestIsRunning()
@@ -117,7 +117,7 @@ class elastest implements Serializable {
 					def counter = 3
 					while (!elastest_is_running && counter > 0){
 						elastest_is_running = waitElastest()
-						def return_status = this.ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
+						def return_status = this.@ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
 						echo 'elastest_is_running:'+elastest_is_running
 						counter = counter -1
 					}
@@ -131,14 +131,14 @@ class elastest implements Serializable {
 				body();	
 				echo '[END] User stages'
 				
-				this.ctx.stage ('release elastest')
+				this.@ctx.stage ('release elastest')
 					stopElastest()
 			}
 		}
 	}
 	
 	def echo (String str){
-		this.ctx.sh 'echo '+str
+		this.@ctx.sh 'echo '+str
 	}
 	
 	def node(String tag){
