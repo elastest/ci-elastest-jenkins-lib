76186e0cb6226cb9fa38d0198d5cfeac1d5085d6
Guiomar <gtunon@naevatec.com>
2017-12-04 10:08:07 +0100

    Update elastest_lib.groovy

--- vars/elastest_lib.groovy
+++ vars/elastest_lib.groovy
@@ -96,7 +96,7 @@ class elastest_lib implements Serializable {
 	def startElastest(){
 		echo '[INI] startElastest'
 		//prepare mem
-		this.@ctx.sh "sysctl -w vm.max_map_count=262144"
+		this.@ctx.sh "sudo sysctl -w vm.max_map_count=262144"
 		
 		def start_elastest_result = this.@ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform:'+this.@version+' start --pullcore '+ this.@mode, returnStatus:true
 		echo 'startElastest-- start_elastest_result = '+start_elastest_result
