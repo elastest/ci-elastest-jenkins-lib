626d1b154291842fedc2b97f3b7797554f5a60e5
gtunon <gtunon@naevatec.com>
2017-12-14 10:19:14 +0100

    refactor and authentication added

--- vars/elastest_lib.groovy
+++ vars/elastest_lib.groovy
@@ -1,59 +1,32 @@
 //With classes
 class elastest_lib implements Serializable {
-	//configuration of the library
-	private boolean verbose = false
 	
+	//Some Contstants
 	private experimental = "--mode=experimental"
 	private experimental_lite = "--mode=experimental-lite"
+
+	//configuration of the library
+	private boolean verbose = false //if the library should echo debug information 
+	private boolean shared = false //if the ElasTest instance is shared
+	def ctx //context of the executing pipeline
+
 	//info of the ElasTest
-	private String ip
+	private String ip= ""
 	private String port="37000"
 	
-	private String sharedElastest_ip = ""
-	private String sharedElastest_user = ""
-	private String sharedElastest_pass = ""
-	
+	//authentication info
+	private String elastest_user = "elastest"
+	private String elastest_pass = ""
+
 	//parameters for the ElasTest
-	private boolean shared = false
 	private String mode = '' //default normal
 	private String version='latest'
-	
-	def ctx
-	
-	
-	/*
-	*	Initialization of the verbose parameter for the ElasTest
-	*/
-	def setVerbose( boolean value) { this.@verbose = value }
-	
-	/*
-	*	Initialization of the version parameter for the ElasTest
-	*/
-	def setVersion( String value) { this.@version = value }
-	
-	/*
-	*	Initialization of the context. It is mandatory for the correct usage of the library
-	*/
-	def setContext(value){ this.@ctx = value }
-	
-	/*
-	*	Initialization of the parameter Lite just for personalization for normal execution leave empty
-	*	for lite execution initialize with '--lite'
-	*/
-	def setMode( String value) { 
-		if (value != null){
-			if (value=="experimental") this.@mode=experimental
-			else if (value=="experimental-lite") this.@mode=experimental_lite
-			else this.@mode=""
-		}
-		else this.@mode=""	
-	}
+	private String is_Authenticated = true
 
-	/*
-	*	Initialization of the shared ElasTest for multiple jobs
-	*/
-	def setShared( boolean value) { this.@shared = value }
 	
+	/*
+	* User methods: Methods to be used in the pipeline to interact with ElasTest
+	*/
 	
 	/*
 	*	@return elastest_ip
@@ -86,30 +59,6 @@ class elastest_lib implements Serializable {
 	
 	
 	/*
-	* checks availability of remote elastest
-	*/
-	def testRemoteElastest(){
-		try {  
-				echo '[INI] testRemoteElastest'		
-				def elastest_url =  'http://'+sharedElastest_ip+':'+this.@port
-
-				echo ' '+elastest_url
-				
-				def response = this.@ctx.httpRequest  authentication: 'nightly_elastest',
-													  consoleLogResponseBody: true,  
-													  ignoreSslErrors: true,
-													  url: elastest_url
-							 				
-				echo '[END] testRemoteElastest'
-				return 0
-				
-			} catch (Exception e) {
-				
-				echo '[END] FAILED: testRemoteElastest '+e
-				return 1
-			}
-		}
-	/*
 	*	Connect container to elastest network
 	*/
 	def connect2ElastestNetwork(){
@@ -123,120 +72,6 @@ class elastest_lib implements Serializable {
 			this.@ctx.sh "docker network connect elastest_elastest "+ containerId
 		}
 		echo '[END] connect2ElastestNetwork'
-	}
-	
-	
-	/*
-	*	Start elastest if it is not running
-	*	@return boolean
-	*/
-	def startElastest(){
-		echo '[INI] startElastest'
-		def start_elastest_result = 1
-		if (! this.@shared){
-			//prepare mem
-			this.@ctx.sh "sudo sysctl -w vm.max_map_count=262144"
-			
-			start_elastest_result = this.@ctx.sh script: 'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform:'+this.@version+' start --pullcore '+ this.@mode, returnStatus:true
-			echo 'startElastest-- start_elastest_result = '+start_elastest_result
-		}
-		else {
-			start_elastest_result = testRemoteElastest()
-		}
-		echo '[END] startElastest'
-		return (start_elastest_result==0)
-	}
-	
-	/*
-	*	Check if ElasTest is running (platform and etm)
-	*	@return boolean
-	*/
-	def elastestIsRunning(){
-		echo '[INI] elastestIsRunning'
-		def elastest_state = false
-		
-		if (! this.@shared == true ){
-			def platform_state = this.@ctx.sh script: 'docker ps | grep elastest_platform | grep -c Up', returnStatus:true
-			def etm_state = this.@ctx.sh script: ' docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' wait --running=0 ', returnStatus:true
-			elastest_state = (platform_state==0 && etm_state==0)
-		}
-		else {
-			def remoteElastest = testRemoteElastest()
-			elastest_state = (remoteElastest==0)
-		}
-		return elastest_state
-		echo '[END] elastestIsRunning'
-	}
-
-	/*
-	*	Wait for Elastest to run (unconfigurable, just the provided wait in the toolbox)
-	*	@return boolean
-	*/
-	def waitElastest(){
-		echo '[INI] elastestIsRunning'
-		def elastest_is_running = false
-		
-		if (! this.@shared == true ){
-			elastest_is_running = this.@ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' wait --container=900', returnStatus:true
-		}
-		else {
-			elastest_is_running = testRemoteElastest()
-		}
-		
-		return (elastest_is_running == 0)
-	}
-
-	/*
-	*	StopElastest if it's running
-	*/
-	def stopElastest(){
-		echo '[INI] stopElastest'
-		if (! this.@shared == true){
-			def stop_elastest_result = this.@ctx.sh script:'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' stop', returnStatus:true
-			def stop_containers_result = this.@ctx.sh script:"docker ps -q |xargs docker rm ", returnStatus:true
-			def delete_images_result = 	 this.@ctx.sh script:"docker images -q |xargs docker rmi -f ", returnStatus:true
-			def delete_volumes_result = this.@ctx.sh script: "docker volume ls |xargs docker volume rm -f", returnStatus:true											
-			echo 'stop_elastest_result = '+(stop_elastest_result &&	stop_containers_result && stop_containers_result && delete_images_result && delete_volumes_result)
-		}		
-		echo '[END] stopElastest'
-	}
-
-	
-	/*
-	*	Get the API
-	*/
-	def getApi(){
-		echo '[INI] getAPI'
-		if ( ! this.@shared == true ){
-			def get_api= this.@ctx.sh (
-					script: 'docker inspect --format=\\"{{.NetworkSettings.Networks.elastest_elastest.Gateway}}\\" elastest_etm-proxy_1',
-					returnStdout: true
-				).trim()
-			echo "ETM container IP=${get_api}"
-			
-			//this is valid for output:
-			/*
-			Waiting for ETM...
-			Platform inserted into network succesfully
-
-			Container created with IP: 172.18.0.19
-			ETM is ready in http://172.18.0.19:8091
-
-			ElasTest API info:
-			Url: http://172.18.0.19:8091
-			*/
-			
-			/*def strings = get_api.tokenize( ':' )
-			this.@ip = strings[strings.size()-2].replaceAll("/",'')
-			this.@port = strings[strings.size()-1]
-			*/
-			this.@ip=get_api
-			this.@ip = this.@ip.replaceAll('"','')
-			echo '[END] getAPI  ip:port ' +this.@ip+':'+this.@port
-		}
-		else {
-			echo '[END] getAPI  ip:port ' +sharedElastest_ip+':'+this.@port
-		}
 	}
 	
 	/*
@@ -253,32 +88,17 @@ class elastest_lib implements Serializable {
 		if (this.@shared == true ){
 			this.@ctx.node('docker'){
 				this.@ctx.stage ('launch elastest')
-					this.@sharedElastest_ip= this.@ctx.sh (
-						script: 'echo $SHARED_ELASTEST_IP',
-						returnStdout: true
-					).trim()
-					this.@sharedElastest_user= this.@ctx.sh (
-						script: 'echo $SHARED_ELASTEST_USER',
-						returnStdout: true
-					).trim()
-					this.@sharedElastest_pass= this.@ctx.sh (
-						script: 'echo $SHARED_ELASTEST_PASS',
-						returnStdout: true
-					).trim()
-					//test environment values
-					//this.@ctx.sh 'env > env.txt'
-					//this.@ctx.sh 'cat env.txt'
-					
+				
 					echo "sharedElastest ="+this.@shared
 					echo "SHARED_ELASTEST_IP:"+sharedElastest_ip
 
-					def elastest_is_running = startElastest()
+					def elastest_is_running = testRemoteElastest()
 					if (! elastest_is_running){
 						this.@ctx.currentBuild.result = 'FAILURE'
 						return
 					}
 					else {
-							getApi()
+							initializeApi()
 					}
 					
 				//body of the pipeline	
@@ -295,7 +115,7 @@ class elastest_lib implements Serializable {
 				this.@ctx.stage ('launch elastest')
 					echo "sharedElastest ="+this.@shared
 					def elastest_is_running = elastestIsRunning()
-					if (elastest_is_running){ //stop and start again --> elastest is unique and frethis.@ctx.sh with each start
+					if (elastest_is_running){ //stop and start again --> elastest is unique and fresh with each start
 						stopElastest()
 						sleep(10)//TODO: change for method like waitToStop or something like that
 						elastest_is_running = elastestIsRunning()
@@ -307,6 +127,7 @@ class elastest_lib implements Serializable {
 					}
 					startElastest()
 					//lets set more time for waiting
+					elastest_is_running = waitElastest();
 					def counter = 3
 					while (!elastest_is_running && counter > 0){
 						elastest_is_running = waitElastest()
@@ -320,7 +141,7 @@ class elastest_lib implements Serializable {
 						return
 					}
 					else {
-							getApi()
+							initializeApi()
 					}
 					
 				//body of the pipeline	
@@ -333,6 +154,199 @@ class elastest_lib implements Serializable {
 			}
 		}
 	}
+	
+	/*
+	*	Initialize unique instance of ElasTest
+	*	@ return boolean
+	*/
+	def startElastest(){
+		echo '[INI] startElastest'
+		def start_elastest_result = 1
+		
+		//prepare mem
+		this.@ctx.sh "sudo sysctl -w vm.max_map_count=262144"
+		
+		if ( this.@is_Authenticated ){
+			//create password 
+			this.@elastest_pass = "elastest_"+this@ctx.env.BUILD_ID+ this@ctx.env.BUILD_NUMBER
+			
+			start_elastest_result = this.@ctx.sh 
+				script: 
+					'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform:'+this.@version+
+					' start --pullcore --user='+this.@elasetest_user+
+					' --password='+this.@elastest_pass+' '+ this.@mode, 
+				returnStatus:true
+		}
+		else {
+			start_elastest_result = this.@ctx.sh 
+				script: 
+					'docker run -d --name="elastest_platform" -v /var/run/docker.sock:/var/run/docker.sock --rm elastest/platform:'+this.@version+
+					' start --pullcore '+ this.@mode, 
+				returnStatus:true
+		}
+		
+		echo 'startElastest-- start_elastest_result = '+start_elastest_result
+		
+		echo '[END] startElastest'
+		return (start_elastest_result==0)
+	}
+	
+	/*
+	* Checks the availability of the Remote shared ElasTest
+	*/
+	def testRemoteElastest(){
+		try {  
+			echo '[INI] testRemoteElastest'		
+			def elastest_url =  'http://'+this.@ip+':'+this.@port
+
+			echo ' '+elastest_url
+			
+			def response = this.@ctx.httpRequest  authentication: 'nightly_elastest',
+												  consoleLogResponseBody: true,  
+												  ignoreSslErrors: true,
+												  url: elastest_url
+										
+			echo '[END] testRemoteElastest'
+			return 0
+			
+		} catch (Exception e) {	
+			echo '[END] FAILED: testRemoteElastest '+e
+			return 1
+		}
+	}
+		
+	/*
+	*	Check if ElasTest exclusive local instance is running (platform and etm)
+	*	@return boolean
+	*/
+	def elastestIsRunning(){
+		echo '[INI] elastestIsRunning'
+		
+		def platform_state = this.@ctx.sh script: 'docker ps | grep elastest_platform | grep -c Up', returnStatus:true
+		def etm_state = this.@ctx.sh script: ' docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' wait --running=0 ', returnStatus:true		
+		
+		echo '[END] elastestIsRunning'
+		
+		return (platform_state==0 && etm_state==0)
+
+	}
+	
+	/*
+	*	Wait for Local exclusive Elastest to run (unconfigurable, just the provided wait in the toolbox)
+	*	@return boolean
+	*/
+	def waitElastest(){
+		echo '[INI] elastestIsRunning'
+		def elastest_is_running = false
+		def counter = 3
+		
+		while (!elastest_is_running && counter > 0){
+			elastest_is_running =  this.@ctx.sh script: 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' wait --container=900', returnStatus:true
+			def return_status = this.@ctx.sh script: 'docker ps | grep elastest_', returnStatus:true
+			elastest_is_running = (return_status == 0 ) && elastest_is_running;
+			echo 'elastest_is_running:'+elastest_is_running
+			counter = counter -1
+		}
+
+		return elastest_is_running 
+	}
+	
+	/*
+	*	Initialize the interface variables (ip, port, user and password) for both shared and exclusive instances
+	*	in order to be retrieved by the pipeline
+	*/
+	def initializeApi(){
+		echo '[INI] getAPI'
+		if ( ! this.@shared == true ){
+			def get_api= this.@ctx.sh (
+					script: 'docker inspect --format=\\"{{.NetworkSettings.Networks.elastest_elastest.Gateway}}\\" elastest_etm-proxy_1',
+					returnStdout: true
+				).trim()
+			echo "ETM container IP=${get_api}"
+
+			this.@ip = get_api
+			this.@ip = this.@ip.replaceAll('"','')
+			echo '[END] getAPI  ip:port ' +this.@ip+':'+this.@port
+		}
+		else {
+			this.@ip= this.@ctx.sh (
+				script: 'echo $SHARED_ELASTEST_IP',
+				returnStdout: true
+			).trim()
+			this.@elastest_user= this.@ctx.sh (
+				script: 'echo $SHARED_ELASTEST_USER',
+				returnStdout: true
+			).trim()
+			this.@elastest_pass= this.@ctx.sh (
+				script: 'echo $SHARED_ELASTEST_PASS',
+				returnStdout: true
+			).trim()
+		}
+		echo '[END] getAPI  ip:port ' +sharedElastest_ip+':'+this.@port
+	}
+	
+	/*
+	*	StopElastest if it's running. 
+	*	In case the instance used is the shared one it does nothing
+	*/
+	def stopElastest(){
+		echo '[INI] stopElastest'
+		if (! this.@shared == true){
+			def stop_elastest_result = this.@ctx.sh script:'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' stop', returnStatus:true
+			def stop_containers_result = this.@ctx.sh script:"docker ps -q |xargs docker rm ", returnStatus:true
+			def delete_images_result = 	 this.@ctx.sh script:"docker images -q |xargs docker rmi -f ", returnStatus:true
+			def delete_volumes_result = this.@ctx.sh script: "docker volume ls |xargs docker volume rm -f", returnStatus:true											
+			echo 'stop_elastest_result = '+(stop_elastest_result &&	stop_containers_result && stop_containers_result && delete_images_result && delete_volumes_result)
+		}		
+		echo '[END] stopElastest'
+	}
+	
+	
+	
+	/*ElasTest Properties configuration*/
+	
+	/*
+	*	Initialization of the version parameter for the ElasTest
+	*/
+	def setVersion( String value) { this.@version = value }
+	
+	/*
+	*	Initialization of the parameter mode just for personalization for normal execution leave empty
+	*	for also accepts: full experimental and experimental-lite execution
+	*/
+	def setMode( String value) { 
+		if (value != null){
+			if (value=="experimental") this.@mode=experimental
+			else if (value=="experimental-lite") this.@mode=experimental_lite
+			else this.@mode=""
+		}
+		else this.@mode=""	
+	}
+	
+	/*
+	*	Authenticated elasTest by default it will use an authenticated instance. 
+	*	For backward compatibility you can override it by setting it to false.
+	*/
+	def setAuthenticatedElastest( boolean value) { this.@is_Authenticated = value }
+	
+	
+	/*Library Initialization*/
+	
+	/*
+	*	Initialization of the verbose parameter for the ElasTest
+	*/
+	def setVerbose( boolean value) { this.@verbose = value }
+	
+	/*
+	*	Initialization of the context. It is mandatory for the correct usage of the library
+	*/
+	def setContext(value){ this.@ctx = value }
+	
+	/*
+	*	Initialization of the shared ElasTest for multiple jobs
+	*/
+	def setShared( boolean value) { this.@shared = value }
+	
 	
 	/*
 	*	Override for echo as if not accessed in the context doesn't work
