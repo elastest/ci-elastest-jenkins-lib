132eddd980b197e19fd8ebab9f9a914519044205
Guiomar <gtunon@naevatec.com>
2018-09-26 11:01:27 +0200

    Update elastest_lib.groovy

--- vars/elastest_lib.groovy
+++ vars/elastest_lib.groovy
@@ -291,12 +291,10 @@ class elastest_lib implements Serializable {
 		
 		def platform_state = this.@ctx.sh script: 'docker ps | grep elastest_platform | grep -c Up', returnStatus:true
 		def etm_state = this.@ctx.sh script: ' docker run --rm -v /var/run/docker.sock:/var/run/docker.sock elastest/platform:'+this.@version+' inspect --api ', returnStatus:true		
-		def tl_volume_c = this.@ctx.sh script: ' docker volume ls | grep -c elastest_etm-testlink', returnStatus:true
-		echo 'tl_volume_c ='+tl_volume_c
 		def folder = this.@ctx.sh script: 'ls  ~/.elastest', returnStatus:true 
 		echo 'folder ='+folder
 		
-		echo '[END] elastestIsStuck : platform_state:'+platform_state+' etm_state:'+etm_state+' tl_container_c: '+ tl_volume_c+ ' .elastest/:'+ folder
+		echo '[END] elastestIsStuck : platform_state:'+platform_state+' etm_state:'+etm_state+ ' .elastest/:'+ folder
 		
 		return (platform_state !=0 || etm_state!=0 || tl_volume_c == '1' || folder !=0 )
 
@@ -367,11 +365,10 @@ class elastest_lib implements Serializable {
 			def stop_containers_result = this.@ctx.sh script:"docker ps -a -f name=elastest -q |xargs docker kill", returnStatus:true
 			def delete_images_result = 	 this.@ctx.sh script:"docker images -q |xargs docker rmi -f ", returnStatus:true
 			def delete_volumes_result = this.@ctx.sh script: "docker volume ls |xargs docker volume rm -f", returnStatus:true											
-			def tl_volume_c = this.@ctx.sh script: ' docker volume ls | grep -c elastest_etm-testlink', returnStdout:true
 			def folder = this.@ctx.sh script: 'ls  ~/.elastest', returnStatus:true 
-			if (tl_volume_c=='1'){
-			      this.@ctx.sh script:"docker volume rm -f elastest_etm-testlink ", returnStatus:true
-			}
+			
+			this.@ctx.sh script:"docker volume rm -f elastest_etm-testlink ", returnStatus:true
+			
 			if (folder==1){
 			      this.@ctx.sh script:"sudo rm -Rf ~/.elastest", returnStatus:true
 			}
